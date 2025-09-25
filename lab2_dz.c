a999044439f611af7884798bac3270366ca22681
nikita krasikov <hdhhdhfggggvv@gmail.com>
2025-09-25 22:43:22 +0300

    lab2_dz.c

--- lab2_dz.c
+++ lab2_dz.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main()
 
 {
+	setlocale(LC_CTYPE, "RUS");
 	int widht_cm = 80;
 	int x = 3;
 	int B = 100;
@@ -10,6 +12,6 @@ int main()
 	float square = x * widht_m;
 	float price = B * square;
 
-	printf("price = %0.f RUB", price);
+	printf("В магазине продается костюмная ткань. Ее цена %d рублей за кв. Метр. Напишите программу, которая подсчитает и выведет на экран стоимость куска этой ткани длиной %d м и шириной 80 см.1. Цена = %0.f рублей", B, x, price);
 	return 0;
 }
